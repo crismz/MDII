9deb703d826b2a22546b67f489a25965ee1b1650
crismuz <crismuz1999@gmail.com>
2023-03-29 23:57:32 -0300

    Avancesitos 3.0

--- src/APIG23.c
+++ src/APIG23.c
@@ -8,6 +8,46 @@
 #define UNUSED(x) (void)(x)
 #define MAX_BUF 128
 #define MAX_VERTICES (u32) 4294967295
+#define hash 1 // Cosito para hash
+
+//Pruebita
+#if hash
+
+typedef struct node {
+  int value;
+  struct node* next;
+} node;
+
+node** create_hash_table(int n) {
+  node** hash_table = malloc(n * sizeof(node*));
+  for (int i = 0; i < n; i++) {
+    hash_table[i] = NULL;
+  }
+  return hash_table;
+}
+
+void insert(node** hash_table, int value, int n) {
+  int idx = value % n;
+  if (hash_table[idx] == NULL) {
+    // if the slot is empty, create a new node and add it to the slot
+    node* new_node = malloc(sizeof(node));
+    new_node->value = value;
+    new_node->next = NULL;
+    hash_table[idx] = new_node;
+  } else {
+    // if the slot is not empty, traverse the linked list and add the new node to the end
+    node* cur_node = hash_table[idx];
+    while (cur_node->next != NULL) {
+      cur_node = cur_node->next;
+    }
+    node* new_node = malloc(sizeof(node));
+    new_node->value = value;
+    new_node->next = NULL;
+    cur_node->next = new_node;
+  }
+}
+#endif
+
 
 /*  Funciones auxiliares para crear grafos */
 static void agregar_vertice(Grafo G, u32 nombre_vertice)
@@ -47,17 +87,63 @@ static void agregar_vecino(Grafo G, vertice v1, vertice v2)
 
 }
 
+static int LeerLado(char *buff, u32 *n, u32 *m){
+    
+    char p;                     // Char para sscanf (Ignorar) 
+    char edge[5];               // String para sscanf (Ignorar)
+    int findEdge = 0;
+    
+    while(buff[0] != 'e' && fgets(buff,1024,stdin) != NULL){
+        if(buff[0] == 'p'){
+            sscanf(buff, "%c %s %u %u",&p, edge, n, m);       
+            findEdge = 1;
+        }   
+    }
+    return findEdge;
+}
+
+
+
+
 Grafo ConstruirGrafo()
 {
+    /*
     //allocar memoria para la estructura principal
-    Grafo g;
+    Grafo g = NULL;
     g = malloc(sizeof(GrafoSt));
     g->num_vertices = 0;
     g->num_lados = 0;
     g->delta = 0;
     g->vertices = NULL;
+    */
 
-    agregar_vertice(g, 1);
+
+
+    char buff[1024];            // Buffer para linea
+    char p;                     // Char para sscanf (Ignorar) 
+    u32 n = 0;                  // Nro de vertices
+    u32 m = 0;                  // Nro de lados
+    u32 v1 = 0;                 // Aux para vertice 1
+    u32 v2 = 0;                 // Aux para vertice 2
+
+    if(!LeerLado(buff,&n,&m)){
+        return NULL;
+    }    
+
+    printf("%u %u",n,m);
+    
+    // Falta agregar que si no lee m lineas debe devolver NULL la estructura
+    for(u32 i = 0; i < m; i++){
+        sscanf(buff, "%c %u %u",&p, &v1, &v2);
+        // Debug  printf("\n\n%u %u\n\n", v1, v2);
+
+
+
+
+        fgets(buff,1024,stdin);
+    }
+    
+   return NULL;
 }
 
 u32 NumeroDeVertices(Grafo G)
--- src/main.c
+++ src/main.c
@@ -1,22 +1,8 @@
 #include "APIG23.h"
 
-int main ()
+int main (int argc, char** argv)
 {
-    char a[1024];
-    int n = 0;
-    int m = 0;
-    char p;
-    char edge[5];
+    Grafo G = ConstruirGrafo();
 
-    while(fgets(a,1024,stdin) != NULL){
-        printf("\n %s",a);
-        if(a[0] == 'p'){
-            sscanf(a, "%c %s %d %d",&p, edge, &n,&m);
-            printf("\n\n\n %d %d \n\n\n",n,m);
-        }
-    }
-
-
-    //Grafo G = ConstruirGrafo();
     return 0;
 }
