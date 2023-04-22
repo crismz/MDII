9de8c35d886f7d95268d04339d72ecf2bb5cdb03
Damian Feigelmuller <69433110+damofeigel@users.noreply.github.com>
2023-04-22 20:07:16 -0300

    perdon, marre
    :dagger:

--- src/APIParte2.c
+++ src/APIParte2.c
@@ -35,20 +35,55 @@ u32 Greedy(Grafo G, u32* Orden, u32* Color)
 }
 
 
-static int comparator(const void *p, const void *q) 
+static int EsPar(u32 n){ return !(n % 2); }
+
+// negativo: p q
+// positivo: q p 
+static int auxComparator(u32 a, u32 b)
 {
-    return ( *(u32*) p - *(u32*) q );
+    if (a > b) return -1;
+    return 1;
 }
 
-int EsPar(u32 n){ return !(n % 2); }
+static int comparator(const void *p, const void *q) 
+{
+    u32 a = *(u32*) p;
+    u32 b = *(u32*) q;
+
+    printf("%u, %u\n", a, b);
+    if ((EsPar(a) && EsPar(b)) || (!EsPar(a) && !EsPar(b))){
+        return auxComparator(a, b);
+    }   
+    if (EsPar(a) && !EsPar(b)) {
+        return 1;
+    } else return -1;
+}
 
 char OrdenImparPar(u32 n, u32* Orden, u32* Color)
 {   
-    qsort(Color, n, sizeof(u32), comparator);
-
-    return 1;
+    for (u32 i=0; i<n; i++){
+        Orden[i] = Color[i];
+    }
+    qsort(Orden, n, sizeof(u32), comparator);
+/*
+    for (u32 i=0; i<n; i++) {
+        printf("\n %u \n", Orden[i]);
+    }
+*/
+    return (char) 0;
 }
 
 char OrdenJedi(Grafo G, u32 *Orden, u32 *Color){
+    
+
+
     return 1;
 }
+
+/*
+orden = [1,0,2,3]
+color = [3,5,4,6] -> [6,5,4,3] 
+      = [0,1,2,3]  
+
+
+*/
